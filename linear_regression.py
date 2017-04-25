e2f4817f177dcf95f072111a6155dc93336a0c6a
Neil Patel <neil.m.patel@rutgers.edu>
2017-04-25 17:19:33 -0400

    Added logistic regression, publishing results of testing and predicting to CSV files

--- linear_regression.py
+++ linear_regression.py
@@ -7,10 +7,15 @@ import csv
 TRAINING_CSV_FILE = 'training-movies.csv'
 TESTING_CSV_FILE = 'upcoming-movies-test.csv'
 PREDICTION_CSV_FILE = 'upcoming-movies-predict.csv'
-MODEL_PREDICTIONS_FILE = 'upcoming-movies-model-predictions.csv'
+LOG_REG_PRED_MODEL_PREDICTIONS_FILE = 'upcoming-movies-pred-model-predictions-logistic-regression.csv'
+LOG_REG_TEST_MODEL_PREDICTIONS_FILE = 'upcoming-movies-test-model-predictions-logistic-regression.csv'
+LIN_REG_PRED_MODEL_PREDICTIONS_FILE = 'upcoming-movies-pred-model-predictions-linear-regression.csv'
+LIN_REG_TEST_MODEL_PREDICTIONS_FILE = 'upcoming-movies-test-model-predictions-linear-regression.csv'
+
 NUM_FEATURES = 21
 RATING_COLUMN_INDEX = 19
-TESTING_SET_PERCENTAGE = 1
+MOVIE_TITLE_INDEX = 9
+MOVIE_LINK_INDEX = 14
 
 '''
 Load training and testing data into numpy arrays
@@ -20,39 +25,68 @@ with open(TRAINING_CSV_FILE, 'rb') as data_file:
 	data = list(data_file_reader)
 	num_rows = len(data)
 
-movies_X = np.empty([num_rows, NUM_FEATURES-1])
-movies_Y = np.empty([num_rows, 1])
+movies_X_train = np.empty([num_rows, NUM_FEATURES-3])
+movies_Y_train = np.empty([num_rows, 1])
 
 with open(TRAINING_CSV_FILE, 'rb') as data_file:
 	data_file_reader = csv.reader(data_file)
 	for index, row in enumerate(data_file_reader):
 		if (index == 0):
 			continue
-		# if (index == 10):
-		# 	break
-		features = [float(row[i]) for i in range(NUM_FEATURES) if i != RATING_COLUMN_INDEX]
+		features = [float(row[i]) for i in range(NUM_FEATURES) if i not in {RATING_COLUMN_INDEX, MOVIE_TITLE_INDEX, MOVIE_LINK_INDEX}]
 		label = float(row[RATING_COLUMN_INDEX])
 		
 		#Append to features and labels arrays
-		movies_X[index] = features
-		movies_Y[index] = label
+		movies_X_train[index] = features
+		movies_Y_train[index] = label
 
-		# print "movies_X[" + str(index) + "] = "
-		# for i in range(NUM_FEATURES - 1):
-		# 	print movies_X[index][i]
-		# print "movies_Y[" + str(index) + "] = " + str(movies_Y[index][0])
+with open(TESTING_CSV_FILE, 'rb') as data_file:
+	data_file_reader = csv.reader(data_file)
+	data = list(data_file_reader)
+	num_test_rows = len(data)-1
 
-training_set_size = int(((100- TESTING_SET_PERCENTAGE) / 100.0) * num_rows)
+movies_X_test = np.empty([num_test_rows, NUM_FEATURES-3])
+movies_Y_test = np.empty([num_test_rows, 1])
+test_output = [['' for col in range(4)] for sample in range(num_test_rows)]
+test_header = []
 
-movies_X_train = movies_X[0:training_set_size, :]
-#print "X_train len: " + str(len(movies_X_train))
-movies_X_test = movies_X[training_set_size:, :]
-#print "X_test len: " + str(len(movies_X_test))
+with open(TESTING_CSV_FILE, 'rb') as data_file:
+	data_file_reader = csv.reader(data_file)
+	for index, row in enumerate(data_file_reader):
+		if (index == 0):
+			test_header = [row[MOVIE_TITLE_INDEX], row[MOVIE_LINK_INDEX], row[RATING_COLUMN_INDEX], "rating_actual"]
+			continue
+		features = [float(row[i]) for i in range(NUM_FEATURES) if i not in {RATING_COLUMN_INDEX, MOVIE_TITLE_INDEX, MOVIE_LINK_INDEX}]
+		label = float(row[RATING_COLUMN_INDEX])
+		sample_info = [row[MOVIE_TITLE_INDEX], row[MOVIE_LINK_INDEX], 0, row[RATING_COLUMN_INDEX]]
 
-movies_Y_train = movies_Y[0:training_set_size]
-#print "Y_train len: " + str(len(movies_Y_train)) 
-movies_Y_test = movies_Y[training_set_size:]
-#print "Y_test len: " + str(len(movies_Y_test))
+		#Append to features and labels arrays
+		movies_X_test[index-1] = features
+		movies_Y_test[index-1] = label
+		test_output[index-1] = sample_info
+
+
+with open(PREDICTION_CSV_FILE, 'rb') as data_file:
+	data_file_reader = csv.reader(data_file)
+	data = list(data_file_reader)
+	num_predict_rows = len(data)-1
+
+movies_X_predict = np.empty([num_predict_rows, NUM_FEATURES-3])
+pred_output = [['' for col in range(3)] for sample in range(num_predict_rows)]
+pred_header = []
+
+with open(PREDICTION_CSV_FILE, 'rb') as data_file:
+	data_file_reader = csv.reader(data_file)
+	for index, row in enumerate(data_file_reader):
+		if (index == 0):
+			pred_header = [row[MOVIE_TITLE_INDEX], row[MOVIE_LINK_INDEX], row[RATING_COLUMN_INDEX]]
+			continue
+		features = [float(row[i]) for i in range(NUM_FEATURES) if i not in {RATING_COLUMN_INDEX, MOVIE_TITLE_INDEX, MOVIE_LINK_INDEX}]
+		sample_info = [row[MOVIE_TITLE_INDEX], row[MOVIE_LINK_INDEX], 0]
+
+		#Append to features and labels arrays
+		movies_X_predict[index-1] = features
+		pred_output[index-1] = sample_info
 
 # Create linear regression model
 regr_model = linear_model.LinearRegression()
@@ -69,43 +103,64 @@ print("Mean squared error: %.3f"
       % np.mean((regr_model.predict(movies_X_test) - movies_Y_test) ** 2))
 # Explained variance score: 1 is perfect prediction
 print('Variance score: %.3f' % regr_model.score(movies_X_test, movies_Y_test))
+lin_test_preds = regr_model.predict(movies_X_test)
+lin_pred_preds = regr_model.predict(movies_X_predict)
 
-# predicted_labels = regr_model.predict(movies_X_test)
-# print('predicted_labels:')
-# for i in range(25):
-# 	print(predicted_labels[i])
+for i in range(len(lin_test_preds)):
+	test_output[i][2] = lin_test_preds[i]
 
-# Re-run logistic regression model with integer labels
+for i in range(len(lin_pred_preds)):
+	pred_output[i][2] = lin_pred_preds[i]
+
+#Write preds to output files
+lin_test_output_file = open(LIN_REG_TEST_MODEL_PREDICTIONS_FILE, "wb")
+writer = csv.writer(lin_test_output_file)
+
+writer.writerow(test_header)
+for i in range(len(test_output)):
+	writer.writerow(test_output[i])
+
+lin_pred_output_file = open(LIN_REG_PRED_MODEL_PREDICTIONS_FILE, "wb")
+writer = csv.writer(lin_pred_output_file)
+
+writer.writerow(pred_header)
+for i in range(len(pred_output)):
+	writer.writerow(pred_output[i])
+
+
+
+# Re-run model with integer labels
 movies_Y_train = [int(round(label)) for label in movies_Y_train]
 movies_Y_test = [int(round(label)) for label in movies_Y_test]
 
-# Create new model
-regr_model_int_labels = linear_model.LinearRegression()
+# Create new model (logistic)
+log_reg_model = linear_model.LogisticRegression(solver='sag', multi_class='multinomial')
 
 # Train new model
-regr_model_int_labels.fit(movies_X_train, movies_Y_train)
+log_reg_model.fit(movies_X_train, movies_Y_train)
 
 print('===== RESULTS WITH ROUNDED INTEGER LABELS =====')
-# The coefficients
-print('Coefficients: \n', regr_model_int_labels.coef_)
-# The mean squared error
-print("Mean squared error: %.3f"
-      % np.mean((regr_model_int_labels.predict(movies_X_test) - movies_Y_test) ** 2))
-# Explained variance score: 1 is perfect prediction
-print('Variance score: %.3f' % regr_model_int_labels.score(movies_X_test, movies_Y_test))
+print('%.3f' % log_reg_model.score(movies_X_test, movies_Y_test))
+log_test_preds = log_reg_model.predict(movies_X_test)
+log_pred_preds = log_reg_model.predict(movies_X_predict)
 
-# predicted_labels = regr_model_int_labels.predict(movies_X_test)
-# print('predicted_labels:')
-# for i in range(25):
-# 	print(predicted_labels[i])
+for i in range(len(log_test_preds)):
+	test_output[i][2] = log_test_preds[i]
 
+for i in range(len(log_pred_preds)):
+	pred_output[i][2] = log_pred_preds[i]
 
-# Plot outputs
-# plt.scatter(movies_X_test, movies_Y_test,  color='black')
-# plt.plot(movies_X_test, regr_model.predict(movies_X_test), color='blue',
-#          linewidth=3)
+# Write preds to output files
+log_test_output_file = open(LOG_REG_TEST_MODEL_PREDICTIONS_FILE, "wb")
+writer = csv.writer(log_test_output_file)
 
-# plt.xticks(())
-# plt.yticks(())
+writer.writerow(test_header)
+for i in range(len(test_output)):
+	writer.writerow(test_output[i])
 
-# plt.show()
+log_pred_output_file = open(LOG_REG_PRED_MODEL_PREDICTIONS_FILE, "wb")
+writer = csv.writer(log_pred_output_file)
+
+writer.writerow(pred_header)
+for i in range(len(pred_output)):
+	writer.writerow(pred_output[i])
